4edf4aa6b6583992a21beb6b05d142634fe0ce80
Nick Healy <nicholas.healy@smarkets.com>
2022-10-18 10:20:08 +0100

    make vanilla server runnable via gunicorn, pre load json data in vanilla server

--- server_no_flask.py
+++ server_no_flask.py
@@ -3,6 +3,8 @@ from wsgiref.simple_server import make_server
 
 complex_res = open('complex_response.json', 'r')
 complex_json = json.load(complex_res)
+complex_bytes = bytes(json.dumps(complex_json), 'utf-8')
+complex_headers = [('Content-Type', 'application/json'), ('Content-Length', str(len(complex_bytes)))]
 
 def application(environ, start_response):
 
@@ -13,10 +15,8 @@ def application(environ, start_response):
     if path == '/simple/':
         data =  b"Hello, World"
     elif path == '/complex/':
-        data = bytes(json.dumps(complex_json), 'utf-8')
-        headers = [('Content-Type', 'application/json'), ('Content-Length', str(len(data)))]
+        data = complex_bytes 
+        headers = complex_headers
     start_response(status, headers)
     return [data]
 
-httpd = make_server('localhost', 8048, application)
-httpd.serve_forever()
